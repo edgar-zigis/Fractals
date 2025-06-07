f5d33b8591d9abc95782b528c1591417f510e0e3
Edgar Žigis <edgar.zigis@gmail.com>
2025-06-07 11:04:52 +0300

    Add Julia fractal

---
+++ julia.cpp
@@ -0,0 +1,241 @@
+﻿//
+//  main.cpp
+//  Mandelbrot & Julia Sets
+//
+//  Created by Edgar Žigis on 29/11/13.
+//  Copyright (c) 2013 Bio:Matic. All rights reserved.
+//
+
+#define GLUT_DISABLE_ATEXIT_HACK
+
+#include "stdafx.h" // uncomment if you use Visual Studio
+#include <stdlib.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#include <QuartzCore/QuartzCore.h>
+#else
+#include <gl/glut.h>
+#include "wtypes.h"
+#endif
+#include <cmath>
+#include <time.h>
+#include <string>
+
+int currScreenWidth, currScreenHeight, c = 0;
+clock_t executionCodeStart;
+
+GLdouble zoomIntensityJulia = 0.1;  // speed of the zoom in Julia set
+GLdouble minXJulia = -1.5f;    // fractal bounds
+GLdouble maxXJulia = 1.5f; // -----||-----
+GLdouble minYJulia = -1.25f; // -----||-----
+GLdouble maxYJulia = 1.25f; // -----||-----
+GLdouble imaginaryXJulia;    // current bound X
+GLdouble imaginaryYJulia;    // current bound Y
+
+GLdouble r, g, b, tr, tg, tb; // coloring parameters
+
+typedef struct
+{
+	GLdouble x;
+	GLdouble y;
+}
+complex;    // definition of our complex number
+
+static complex julianCoef;
+
+
+void renderJulia(void)
+{
+	int x, y, cnt, maxIter;
+	complex z, t, t2;
+	r = g = b = 256.0;
+	maxIter = 256.0;
+	imaginaryXJulia = (maxXJulia - minXJulia) / (GLdouble)currScreenWidth;
+	imaginaryYJulia = (maxYJulia - minYJulia) / (GLdouble)currScreenHeight;
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+	glBegin(GL_POINTS);
+
+	for (x = 0, z.x = minXJulia; x < currScreenWidth; x++, z.x += imaginaryXJulia)
+	{
+		for (y = 0, z.y = minYJulia; y < currScreenHeight; y++, z.y += imaginaryYJulia)
+		{
+			t2 = z;
+			cnt = 0;
+
+			while ((t2.x * t2.x + t2.y * t2.y <= 4) && (cnt < maxIter))
+			{
+				t.x = t2.x * t2.x - t2.y * t2.y;
+				t.y = 2 * t2.x * t2.y;
+				t2 = t;
+				t2.x += julianCoef.x;   // x0 is constant
+				t2.y += julianCoef.y;   // y0 is constant too
+				cnt++;
+			}
+
+			if (cnt != maxIter)
+			{
+				if (cnt >= 0 && cnt <= 31) { b = cnt * 4; g = cnt * 8; r = 0; }
+				else if (cnt >= 32 && cnt <= 63) { b = 200; g = 500 - cnt * 8; r = 0; }
+				else if (cnt >= 64 && cnt <= 95) { b = 200; g = 0; r = (cnt - 64) * 4; }
+				else if (cnt >= 96 && cnt <= 127) { r = 200; g = 0; b = 1000 - cnt * 8; }
+				else if (cnt >= 128 && cnt <= 159) { r = 200; g = (cnt - 128) * 8; b = 0; }
+				else if (cnt >= 160 && cnt <= 191) { g = 200; r = 1500 - cnt * 8; b = 0; }
+				else if (cnt >= 192 && cnt <= 223) { g = 200; r = 0; b = (cnt - 192) * 8; }
+				else if (cnt >= 224 && cnt <= 255) { g = 230; r = (cnt - 224) * 8; b = 256; }
+
+				tr = r; tb = b; tg = g;
+				switch (c)
+				{
+				case 0:
+					break;
+				case 1: r = tb; b = tr;
+					break;
+				case 2: r = tg; g = tr;
+					break;
+				case 3: b = tg; g = tb;
+					break;
+				case 4: r = tg; g = tb; b = tr;
+					break;
+				case 5: r = tb; g = tr; b = tg;
+					break;
+				}
+				glColor3f(r / 256, g / 256, b / 256);
+				glVertex3d(x - currScreenWidth / 2, y - currScreenHeight / 2, 0.0f);
+			}
+		}
+	}
+
+	glEnd();
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void juliaSpecKeyboardControl(int key, int x, int y)
+{
+	if (key == GLUT_KEY_UP)
+	{
+		maxYJulia += zoomIntensityJulia;
+		minYJulia += zoomIntensityJulia;
+	}
+	if (key == GLUT_KEY_DOWN)
+	{
+		maxYJulia -= zoomIntensityJulia;
+		minYJulia -= zoomIntensityJulia;
+	}
+	if (key == GLUT_KEY_RIGHT)
+	{
+		maxXJulia += zoomIntensityJulia;
+		minXJulia += zoomIntensityJulia;
+	}
+	if (key == GLUT_KEY_LEFT)
+	{
+		maxXJulia -= zoomIntensityJulia;
+		minXJulia -= zoomIntensityJulia;
+	}
+
+	glutPostRedisplay();
+}
+
+void juliaKeyboardControl(unsigned char key, int x, int y)
+{
+
+	GLdouble zoomCoef = 0.833333333333333333333333;
+
+	if (key == 'q' || key == 'Q')
+	{
+		if (maxXJulia - minXJulia>3 * zoomIntensityJulia)
+		{
+			maxXJulia -= zoomIntensityJulia;
+			minXJulia += zoomIntensityJulia;
+			maxYJulia -= zoomIntensityJulia * zoomCoef;
+			minYJulia += zoomIntensityJulia * zoomCoef;
+		}
+		else
+			zoomIntensityJulia = zoomIntensityJulia / 10;
+	}
+	if (key == 'a' || key == 'A')
+	{
+		maxXJulia += zoomIntensityJulia;
+		minXJulia -= zoomIntensityJulia;
+		maxYJulia += zoomIntensityJulia*zoomCoef;
+		minYJulia -= zoomIntensityJulia*zoomCoef;
+	}
+	if (key == 'c' || key == 'C')
+	{
+		c++;
+		if (c == 6)
+			c = 0;
+	}
+	if (key == 'w')
+	{
+		zoomIntensityJulia /= 10.0f;
+	}
+	if (key == 's')
+	{
+		zoomIntensityJulia *= 10.0f;
+	}
+	glutPostRedisplay();
+}
+
+void reshape(int w, int h)
+{
+	currScreenWidth = w;
+	currScreenHeight = h;
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-w / 2, w / 2, -w / 2, w / 2, -w / 2, w / 2);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void init()
+{
+	julianCoef.x = 0.36f;
+	julianCoef.y = 0.36f;
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0f, 1.0f, 0.0f);
+}
+
+void GetDesktopResolution(int& horizontal, int& vertical)   // uncomment if you use Windows OS
+{
+RECT desktop;
+const HWND hDesktop = GetDesktopWindow();
+GetWindowRect(hDesktop, &desktop);
+horizontal = desktop.right;
+vertical = desktop.bottom;
+}
+
+/*void GetDesktopResolution(int& horizontal, int& vertical)   // uncomment if you use Mac OS
+{
+	CGRect mainMonitor = CGDisplayBounds(CGMainDisplayID());
+	horizontal = CGRectGetWidth(mainMonitor);
+	vertical = CGRectGetHeight(mainMonitor);
+}
+*/
+
+int main(int argc, char* argv[])
+{
+
+	int width;
+	int height;
+	GetDesktopResolution(width, height);
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(700, 700);
+
+	glutCreateWindow("Edgaro Žigio Julijos aibė");
+	glutPositionWindow((width - 700) / 2, (height - 700) / 2);
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(renderJulia);
+	glutSpecialFunc(juliaSpecKeyboardControl);
+	glutKeyboardFunc(juliaKeyboardControl);
+	init();
+	glutMainLoop();
+
+	return 0;
+}
